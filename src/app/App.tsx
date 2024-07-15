a86e0cba26aa6ccd71a995d1812781396b300c9c
Irina Fileva <irinkaskai@gmail.com>
2024-07-15 15:28:41 +0500

    fix: relocate ErrorBoundary

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -1,7 +1,7 @@
 import { FC } from 'react';
 import { RouterProvider } from 'react-router-dom';
 import { router } from './providers/routerProvider';
-import { ErrorBoundary } from 'shared/lib/ui/ErrorBoundary';
+import { ErrorBoundary } from './providers/errorBoundary';
 
 export const App: FC = () => {
   return (
