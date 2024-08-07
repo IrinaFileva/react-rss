68e250c86124f88d677cd9cce080d3f53e39e77f
Irina Fileva <irinkaskai@gmail.com>
2024-08-07 11:37:20 +0500

    fix: delete pages router

--- src/shared/test/ui/404.test.tsx
+++
@@ -1,9 +0,0 @@
-import { render } from '@testing-library/react';
-import NotFound from 'pages/404';
-
-it('testing 404 page', () => {
-  const { getByText } = render(<NotFound />);
-
-  const text = getByText(/404/);
-  expect(text).toBeInTheDocument();
-});
